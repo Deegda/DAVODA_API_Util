a2091829acccdeba3d2bca49b2a6585c7d282853
aaron <mspringmspring@naver.com>
2021-12-10 01:22:32 +0900

    feat: config

--- release.config.js
+++ release.config.js
@@ -4,7 +4,7 @@ module.exports = {
         [
             '@semantic-release/npm',
             {
-                npmPublish: false
+                npmPublish: true
             }
         ],
         [
