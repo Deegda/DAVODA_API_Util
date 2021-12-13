502db3cde4eee568883935cb1810d43b6e8212d0
yangseungbin <tanja1206@naver.com>
2021-12-13 23:24:22 +0900

    feat: aws util

---
+++ src/aws/index.ts
@@ -0,0 +1,6 @@
+export * from './service/ses.service';
+export * from './service/s3.service';
+export * from './interfaces/ses.options';
+export * from './interfaces/s3.options';
+export * from './constant';
+export * from './aws.module';
