697dd61576b1e2dbc1490b270d6a11f208ca6395
Myeongcheol Yu <mspringmspring@naver.com>
2022-01-17 18:50:33 +0900

    fix: semantic release config

--- release.config.js
+++ release.config.js
@@ -2,23 +2,18 @@ module.exports = {
     branches: ['master', 'next'],
     plugin: [
         [
+            '@semantic-release/npm',
+            {
+                tarballDir: 'dist'
+            }
+        ],
+        [
             '@semantic-release/github',
             {
                 successComment:
                     ':tada: This ${issue.pull_request ? "PR is included" : "issue has been resolved"} in' +
                     '[version ${nextRelease.version}](${releases.filter(release => /github.com/i.test(release.url))[0].url}) :tada:',
-                assets: [
-                    {
-                        path: 'dist.zip',
-                        label: 'dist-${nextRelease.gitTag}.zip'
-                    }
-                ]
-            }
-        ],
-        [
-            '@semantic-release/npm',
-            {
-                tarballDir: 'dist-${nextRelease.gitTag}.zip'
+                assets: 'dist/*.tgz'
             }
         ],
         [
