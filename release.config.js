4390f33617cd0334a8412a6e2e9683522d84323d
Myeongcheol Yu <mspringmspring@naver.com>
2022-01-17 18:23:24 +0900

    fix: release config

--- release.config.js
+++ release.config.js
@@ -1,13 +1,19 @@
 module.exports = {
     branches: ['master', 'next'],
     plugin: [
-        ['@semantic-release/npm', { tarballDir: 'dist' }],
+        ['@semantic-release/npm', { tarballDir: 'dist-${nextRelease.gitTag}' }],
         [
             '@semantic-release/github',
             {
                 successComment:
                     ':tada: This ${issue.pull_request ? "PR is included" : "issue has been resolved"} in' +
-                    '[version ${nextRelease.version}](${releases.filter(release => /github.com/i.test(release.url))[0].url}) :tada:'
+                    '[version ${nextRelease.version}](${releases.filter(release => /github.com/i.test(release.url))[0].url}) :tada:',
+                assets: [
+                    {
+                        path: 'dist',
+                        label: 'dist-${nextRelease.gitTag}'
+                    }
+                ]
             }
         ],
         [
