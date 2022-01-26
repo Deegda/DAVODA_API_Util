878eac93f45eb3b34b36644c1156a58c81851360
seungbin9850 <tanja1206@naver.com>
2022-01-26 23:21:56 +0900

    fix: aws module 수정

--- src/aws/aws.module.ts
+++ src/aws/aws.module.ts
@@ -13,7 +13,8 @@ export class AwsModule {
                     provide: CONFIG_CONNECTION_OPTIONS,
                     useFactory: options.useFactory,
                     inject: options.inject || []
-                }
+                },
+                S3Service
             ],
             exports: [S3Service]
         };
